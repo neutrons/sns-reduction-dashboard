ba785f446db64204a6601af885e9b974beed5999
Tanner Hobson <thobson125@gmail.com>
2016-07-15 17:16:24 -0400

    Breaking a few things before I add better catalog support

--- src/components/CatalogRoute.vue
+++ src/components/CatalogRoute.vue
@@ -1,70 +1,51 @@
 <template>
-    <div>
-        <div class="col-xs-9">
-            <panel>
-                <span slot="title">Title</span>
-                <div v-loading="$loadingRouteData">
-                    <table class="table table-hover" v-if="status==='success'">
-                        <thead>
-                            <tr>
-                                <th>Run</th>
-                                <th>Title</th>
-                                <th>Start</th>
-                                <th>End</th>
-                                <th>Duration</th>
-                                <th>Proton</th>
-                                <th>Total</th>
-                                <th>Reduce</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="entry in data.entries">
-                                <td>{{ entry.run }}</td>
-                                <td>{{ entry.title }}</td>
-                                <td v-time-ago="entry.start_time"></td>
-                                <td v-time-ago="entry.end_time"></td>
-                                <td v-time-diff="entry.duration"></td>
-                                <td>{{ entry.proton_charge }}</td>
-                                <td>{{ entry.total_counts }}</td>
-                                <td>
-                                    <a href="#!">
-                                        <span class="glyphicon glyphicon-download-alt"></span>
-                                    </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <!-- pagination -->
-                    <div class="text-center">
-                        <ul class="pagination">
-                            <li class="disabled">
-                                <span>&laquo;</span>
-                            </li>
-                            <li class="active">
-                                <span>1</span>
-                            </li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            <li><a href="#">5</a></li>
-                            <li><a href="#">&raquo;</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </panel>
+  <div class="row">
+    <div class="col-xs-9">
+      <panel>
+        <span slot="title">Facility</span>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-3 col-xs-offset-3">
+              <button class="btn btn-block btn-default">SNS</button>
+            </div>
+            <div class="col-xs-3">
+              <button class="btn btn-block btn-default">HFIR</button>
+            </div>
+          </div>
         </div>
-        <div class="col-xs-3">
-            <sidebar-panel>
-                <span slot="title">Catalog</span>
-                <div slot="body" class="list-group">
-                    <a href="#" class="list-group-item ">see experiments</a>
-                    <a href="#" class="list-group-item active">see IPTS-11255</a>
-                    <a href="#" class="list-group-item">runs</a>
-                    <a href="#" class="list-group-item disabled">this is disabled</a>
-                </div>
-            </sidebar-panel>
+      </panel>
+
+      <panel v-if="hasFacility">
+        <span slot="title">Instrument</span>
+        <div class="container-fluid">
+          <div class="row">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="instrument in instruments">
+                  <td>{{instrument.name}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </panel>
     </div>
+    <div class="col-xs-3">
+      <sidebar-panel>
+        <span slot="title">Catalog</span>
+        <div class="list-group-item" slot="body">
+          <a class="list-group-item" href="">Item 001</a>
+          <a class="list-group-item" href="">Item 002</a>
+          <a class="list-group-item" href="">Item 003</a>
+        </div>
+      </sidebar-panel>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -78,30 +59,30 @@ import { catalog } from '../resource';
 import { route } from '../vuex/getters';
 
 export default {
-    name: 'CatalogRoute',
-    components: {
-        SidebarPanel,
-        Panel,
+  name: 'CatalogRoute',
+  components: {
+    SidebarPanel,
+    Panel,
+  },
+  directives: {
+    TimeAgo,
+    TimeDiff,
+    loading,
+  },
+  data() {
+    return { status: 'pending', data: null, message: null, hasFacility: true, instruments: [ {name:'Foo'}, {name:'Bar'} ] };
+  },
+  vuex: {
+    getters: {
+      route,
     },
-    directives: {
-        TimeAgo,
-        TimeDiff,
-        loading,
+  },
+  route: {
+    data({ next }) {
+      catalog.get({page: this.route.params.page}).then(response => {
+        next(response.json());
+      });
     },
-    data() {
-        return { status: 'pending', data: null, message: null };
-    },
-    vuex: {
-        getters: {
-            route,
-        },
-    },
-    route: {
-        data({ next }) {
-            catalog.get({page: this.route.params.page}).then(response => {
-                next(response.json());
-            });
-        },
-    },
+  },
 };
 </script>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="container">
-        <div class="row main">
-            <router-view></router-view>
-        </div>
+        <router-view></router-view>
     </div>
 </template>
 
