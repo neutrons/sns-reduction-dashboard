85cf8b9d6518a9f146d96c856add19d9a044af41
Tanner Hobson <thobson125@gmail.com>
2016-07-18 13:03:24 -0400

    Webpack with hot reloading

--- Makefile
+++ Makefile
@@ -15,13 +15,31 @@ ifndef MANAGEPY
 MANAGEPY := $(PYTHON) manage.py
 endif
 
+ifndef NODE
+NODE := $(firstword $(shell which node nodejs 2>/dev/null))
+endif
+
 # Local variables
 
 # Standard targets
 
 .PHONY: all
 all:
+	+$(MAKE) -j 2 server-webpack server-django
 
 # Application specific targets
+
+.PHONY: migrate
+migrate:
+	$(MANAGEPY) makemigrations
+	$(MANAGEPY) migrate
+
+.PHONY: server-webpack
+server-webpack:
+	$(NODE) server.js
+
+.PHONY: server-django
+server-django:
+	$(MANAGEPY) runserver 8888
 
 # Source transformations
--- configuration/views.py
+++ configuration/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import generic
+from django.conf import settings
+
+import json
+import time
 
 # Create your views here.
+
+def json_load_file_until_success(filename):
+    while True:
+        try:
+            with open(filename, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            time.sleep(0.5)
+
+class IndexView(generic.TemplateView):
+    template_name = 'configuration/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['assets'] = json_load_file_until_success(settings.ASSETS_FILE)
+        return context
