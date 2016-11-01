c857d45f29406a9ffde6d5cba516f2135837788e
Tanner Hobson <thobson125@gmail.com>
2016-11-01 16:06:00 -0400

    Add label and help text to form fields

--- api/api/reduction/forms.py
+++ api/api/reduction/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from .models import Configuration
 
 
 class ConfigurationParametersForm(forms.Form):
-    absolute_scale_factor = forms.FloatField()
-    sample_thickness = forms.FloatField()
+    absolute_scale_factor = forms.FloatField(
+        required=True,
+        label=_('Absolute Scale Factor'),
+        initial=1.0,
+        help_text=_('The factor used when scaling the readings from the sensor.'),
+    )
+
+    sample_thickness = forms.FloatField(
+        required=True,
+        label=_('Sample Thickness'),
+        initial=0.1,
+        help_text=_('The thickness of the sample in mm'),
+    )
